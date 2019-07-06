260ed6aca5037f19c2d1698f3a668c3320e9d216
sameshen <as285996985@126.com>
2019-07-06 21:27:06 +0800

    test

---
+++ src/main/java/com/beacon/test1/App.java
@@ -0,0 +1,16 @@
+package com.beacon.test1;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        String responseStr = "";
+        JSONObject object = JSON.parseObject(responseStr);
+    }
+}
